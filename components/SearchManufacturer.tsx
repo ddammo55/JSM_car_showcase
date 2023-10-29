fa43efd2abc9fb2181d6ec21a67cb75bf8903542
ddammo55 <ddammo55@naver.com>
2023-10-30 06:29:40 +0900

    first commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,11 @@
+import { SearchManufacturerProps } from '@/type';
+import React from 'react';
+
+export default function SearchManufacturer({manufacturer, setManufacturer}: SearchManufacturerProps) {
+    return (
+        <div>
+            SearchManufacturer
+        </div>
+    );
+}
+
---
+++ type/index.ts
@@ -0,0 +1,14 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    btnType?: "button" | "submit";
+    containerStyles? :string;
+    handleClick?:
+    MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
